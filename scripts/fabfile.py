4806a047192a9abb25452afdfe67a94e33a60756
Tony Angerilli <tony@7geese.com>
2014-10-13 15:01:45 -0700

    deployment support

---
+++ scripts/fabfile.py
@@ -0,0 +1,32 @@
+from fabric.api import *
+from fabric.contrib.files import *
+
+
+@task
+def deploy():
+    sudo("apt-get update")
+    sudo("apt-get install -y python-pip python-virtualenv")
+    sudo("apt-get install -y redis-server")
+    append("/etc/redis/redis.conf", "save 60 1", use_sudo=True)
+    sudo("/etc/init.d/redis-server restart")
+
+    run("virtualenv .env")
+    run(".env/bin/pip install redis BeautifulSoup supervisor")
+    put("search.py")
+    run("mkdir -p lib")
+    put("lib/realtylink.py", "lib/")
+    run("touch lib/__init__.py")
+    put("supervisord.conf")
+
+
+@task
+def start():
+    sudo("/etc/init.d/redis-server start")
+    run(".env/bin/supervisord -c /home/ubuntu/supervisord.conf")
+
+
+@task
+def get_db():
+    sudo('cp /var/lib/redis/dump.rdb /tmp')
+    sudo('chmod a+r /tmp/dump.rdb')
+    get('/tmp/dump.rdb')
